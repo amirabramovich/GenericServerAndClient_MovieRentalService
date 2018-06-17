95704f27699b8f23042fdc41afb2ccd929d719ce
amirabramovich <30535864+amirabramovich@users.noreply.github.com>
2018-06-17 20:56:22 +0300

    Update MaxHeapComp.java

--- GenericServerAndClient_MovieRentalService/Server/src/main/java/bgu/spl181/net/srv/bidi/MaxHeapComp.java
+++ GenericServerAndClient_MovieRentalService/Server/src/main/java/bgu/spl181/net/srv/bidi/MaxHeapComp.java
@@ -3,9 +3,7 @@ package bgu.spl181.net.srv.bidi;
 import java.util.Comparator;
 /**
  * This class represents the comparator we use in our heapqueue managing the movie ids
- * Each time movie added we give ti maxid+1
- * @author chen
- *
+ * Each time movie added we give it maxid+1
  */
 public class MaxHeapComp implements Comparator<Integer> {
 
